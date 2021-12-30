bd2310893658741ec32d2d694354b201459a4e88
Mohamed Alaa <mohamedalaashafik98@gmail.com>
2021-12-30 03:00:50 +0200

    Payment System V1.3

--- Declarations.h
+++ Declarations.h
@@ -31,15 +31,6 @@ typedef enum EN_transStat_t
 	APPROVED
 }EN_transStat_t;
 
-ST_cardData_t customer; //Declaration of variable of the ST_cardData_t type
-ST_terminalData_t data1; //Declaration of variable of the ST_terminalData_t type
-
-/*Declaration of global array that holds customers' information*/
-ST_accountBalance_t g_arr[10]={{100.00,"123456789"},{6000.0,"234567891"},{3250.25,"567891234"},{1500.12,"456789123"},{500.00,"258649173"},{2100.00,"654823719"},
-								{0.00,"971362485"},{1.00,"793148625"},{10.12,"123123456"},{0.55,"456789321"}};
-
-EN_transStat_t result; //Declaration of variable of the ST_transStat_t type
-uint8_t gu8_size=sizeof(g_arr)/sizeof(ST_accountBalance_t);// size of the array
 
 /*Declaration of a function that check the card is expired or not and return the result */
 uint8_t Check_cardExp(ST_cardData_t customer,ST_terminalData_t data1);
